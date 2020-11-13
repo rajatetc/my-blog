715241a43cf92841d977e1262ed705fb0cadafa3
Rajat Gupta <rajatgupta1798@gmail.com>
2020-11-13 21:42:34 +0530

    added contact form

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,10 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `Contact`,
+            slug: `/contact`,
+          },
         ],
         externalLinks: [
           {
