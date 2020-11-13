4ee411b71a38a373e3b0899b402f938b50765243
Rajat Gupta <rajatgupta1798@gmail.com>
2020-11-13 22:11:04 +0530

    deleting contact form

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,10 +38,6 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
-          {
-            title: `Contact`,
-            slug: `/contact`,
-          },
         ],
         externalLinks: [
           {
