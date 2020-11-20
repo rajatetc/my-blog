e0fab6e56cd827cdc73d06c3efef31d11b2f35bd
Rajat Gupta <rajatgupta1798@gmail.com>
2020-11-20 17:36:20 +0530

    contact form addition

---
+++ content/pages/contact/contact-form.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ContactForm = () => {
+    return (
+        <>
+            <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+                <p>
+                    <label>Email: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <div data-netlify-recaptcha="true"></div>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+
+        </>
+    )
+}
+
+export default ContactForm;
+
